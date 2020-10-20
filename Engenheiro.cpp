8ff3ab1723829036d03a2b903d51ffe8cb60dea7
CogitoRat <73200341+CogitoRat@users.noreply.github.com>
2020-10-20 18:31:12 -0300

    Update Engenheiro.cpp

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,11 +1,10 @@
+ 
 #include <string>
 #include "Empregado.hpp"
 
 class Engenheiro : public Empregado {
-
   public:
-    std::string nome;  
-	int projetos;
-	
+    Engenheiro(std::string _nome, double _salarioHora, int _projetos) : Empregado(_nome, _salarioHora), projetos(_projetos){}
+    int projetos;	
 };
 
